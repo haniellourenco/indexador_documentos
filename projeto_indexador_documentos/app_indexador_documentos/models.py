6c808cd5e739ea09cb07fa086f61c0839324ce22
Haniel <hanielohn2016@gmail.com>
2024-06-22 23:33:50 -0300

    add registro de documento com bd

--- projeto_indexador_documentos/app_indexador_documentos/models.py
+++ projeto_indexador_documentos/app_indexador_documentos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Documento(models.Model):
+    id_documento = models.AutoField(primary_key=True)
+    nome_documento = models.TextField(max_length=255)
+    data_modificacao = models.DateField(auto_now=True)
--- projeto_indexador_documentos/projeto_indexador_documentos/urls.py
+++ projeto_indexador_documentos/projeto_indexador_documentos/urls.py
@@ -4,4 +4,5 @@ from app_indexador_documentos import views
 urlpatterns = [
     # rota, view responsável, nome de referência
     path('', views.home, name='home'),
+    path('documentos/', views.documentos, name='listagem_documentos'),
 ]
