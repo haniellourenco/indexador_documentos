ec5b6b7628bbb76db62b4f64d9f7930cf9fe7301
Haniel <hanielohn2016@gmail.com>
2024-06-25 20:31:15 -0300

    add nav pagina lista documentos

--- projeto_indexador_documentos/app_indexador_documentos/views.py
+++ projeto_indexador_documentos/app_indexador_documentos/views.py
@@ -5,14 +5,16 @@ def home(request):
     return render(request,'documentos/home.html')
 
 def documentos(request):
-    #Salvar os dados da tela para o banco de dados.
-    novo_documento = Documento()
-    novo_documento.nome_documento = request.POST.get('nome')
-    novo_documento.data_modificacaodata = request.POST.get('data')
-    novo_documento.save()
-    #Exibir todos os documentos já inseridos em uma nova página
+    if request.method == 'POST':
+        # Salvar os dados da tela para o banco de dados
+        novo_documento = Documento()
+        novo_documento.nome_documento = request.POST.get('nome')
+        novo_documento.data_modificacao = request.POST.get('data')
+        novo_documento.save()
+    
+    # Exibir todos os documentos já inseridos em uma nova página
     documentos = {
         'documentos': Documento.objects.all()
     }
-    #Retornar os dados para a página de listagem de documentos
-    return render(request,'documentos/documentos.html',documentos)
+    # Retornar os dados para a página de listagem de documentos
+    return render(request, 'documentos/documentos.html', documentos)
